64f9c4306bc0126d07684ab26d8b057f38d2d160
Antonio Ren <antonio.ren@epita.fr>
2019-07-22 21:47:03 +0200

    fonction info added

--- application/djangoapp/views.py
+++ application/djangoapp/views.py
@@ -3,5 +3,9 @@ from apipkg import api_manager as api
 
 
 def index(request):
-    time = api.send_request('scheduler', 'clock/time')
-    return HttpResponse("L'heure de la clock est %r" % time)
+    info = api.send_request('business-intelligence', 'info')
+    return HttpResponse("Je suis gestion des stock et je reçois %r" % info)
+
+def info(request):
+    return HttpResponse("Je suis gestion des stocks")
+
