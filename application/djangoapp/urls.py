9be43086a0c27dbe5cdc61696fe8c07f6616805d
ClaireHayard <claire.hayard@epita.fr>
2019-09-30 16:10:31 +0200

    clean code

--- application/djangoapp/urls.py
+++ application/djangoapp/urls.py
@@ -9,9 +9,6 @@ urlpatterns = [
     path('list/update', views.get_product, name='list_update'),
     path('list/delete', views.list_delete, name='list_delete'),
     path('schedule/add', views.add_schedule, name='add_schedule'),
-    path('add-article/', views.add_article, name='add-article'),
-    path('remove-article/', views.remove_article, name='remove-article'),
-    path('clear/', views.clear, name='clear'),
     path('schedule/', views.schedule, name='schedule'),
     path('schedule/demo', views.demo_schedule, name='demo-schedule'),
 ]
--- application/djangoapp/views.py
+++ application/djangoapp/views.py
@@ -12,12 +12,15 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+#Affichage de la page d'accueil
 def index(request):
     return render(request, "index.html")
 
+#...
 def info(request):
     return HttpResponse("Gestion des stocks")
 
+#
 def demo_schedule(request):
     clock_time = api.send_request('scheduler', 'clock/time')
     time = datetime.strptime(clock_time, '"%d/%m/%Y-%H:%M:%S"')
@@ -25,36 +28,6 @@ def demo_schedule(request):
     time_str = time.strftime('%d/%m/%Y-%H:%M:%S')
     schedule_task('gestion-stock', '/list/update','minute','','demo', time_str)
     return HttpResponseRedirect('/schedule')
-
-def add_schedule(request):
-    if request.method == 'POST':
-        form = ScheduleForm(request.POST)
-        if form.is_valid():
-            schedule_task(form['host'].value(),
-                          form['url'].value(),
-                          form['recurrence'].value(),
-                          form['data'].value(),
-                          form['name'].value(),
-                          form['time'].value())
-            return HttpResponseRedirect('/schedule')
-        else:
-            form = ScheduleForm()
-        return render(request, 'index.html', {'form': form})
-
-def add_article(request):
-    if request.method == 'POST':
-        form = ArticleForm(request.POST)
-        if form.is_valid():
-            first = Article.objects.all().filter(nom=form['nom'].value()).first()
-            if (first is None) :
-                new_article = form.save()
-            else :
-                first.stock += int(form['stock'].value())
-                first.save()
-            return HttpResponseRedirect('/list')
-    else:
-        form = ArticleForm()
-    return render(request, 'add_article.html', {'form' : form})
 
 def list(request):
     context = {
@@ -103,6 +76,21 @@ def get_product(request):
         return HttpResponseRedirect('/list')
 
 
+def add_schedule(request):
+    if request.method == 'POST':
+        form = ScheduleForm(request.POST)
+        if form.is_valid():
+            schedule_task(form['host'].value(),
+                          form['url'].value(),
+                          form['recurrence'].value(),
+                          form['data'].value(),
+                          form['name'].value(),
+                          form['time'].value())
+            return HttpResponseRedirect('/schedule')
+        else:
+            form = ScheduleForm()
+        return render(request, 'index.html', {'form': form})
+
 def schedule_task(host, url, recurrence,data, name, time):
     headers = {'Host': 'scheduler'}
     data = {"target_url": url, "target_app": host, "time": time, "recurrence": recurrence, "data": data, "source_app": "gestion-stock", "name": name}
@@ -111,34 +99,14 @@ def schedule_task(host, url, recurrence,data, name, time):
     print(r.text)
     return r.text
 
-def log(request):
-    date = datetime.now().strftime('%Y-%m-%d-%H-%M')
-    #FIXME ajouter le log dans la BDD
-    return list(request)
-
-def clear(request):
-    Article.objects.all().delete()
-    return HttpResponseRedirect('/list')
-
-def remove_article(request):
-    if request.method == 'POST':
-        form = ArticleForm(request.POST)
-        if form.is_valid():
-            first = Article.objects.all().filter(nom=form['nom'].value()).first()
-            if (first is not None) :
-                form_stock = int(form['stock'].value())
-                if (form_stock > first.stock):
-                    first.stock = 0
-                else :
-                    first.stock -= int(form['stock'].value())
-                first.save()
-            return HttpResponseRedirect('/list')
-    else:
-        form = ArticleForm()
-    return render(request, 'remove_article.html', {'form' : form})
-
 def schedule(request):
     list = api.send_request('scheduler', 'schedule/list')
     time = api.send_request('scheduler', 'clock/time')
     return render(request, "schedule.html", {'list' : list, 'time':time, 'form': ScheduleForm})
 
+
+
+#FIXME ajouter le log dans la BDD
+def log(request):
+    date = datetime.now().strftime('%Y-%m-%d-%H-%M')
+    return list(request)
